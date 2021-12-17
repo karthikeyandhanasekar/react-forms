d844358b934f4cae72040c0949a872b87b8d80f2
Karthik <dkkarthik2000@gmail.com>
2021-12-17 11:42:53 +0530

    changes

--- src/components/display.jsx
+++ src/components/display.jsx
@@ -21,12 +21,12 @@ usertype: "personal" */
 const Display = () => {
     const form = useSelector(state => state.forms)
     const keys = Object.keys(form)[0];
+
     if (keys) {
-        const data = Object.values(form[keys]).map((ele, index) => {
-            if (index > 1) {
-                return <div className='data' key={index}>{ele}</div>
-            }
-        })
+        const data = Object.keys(form[keys]).map((ele, index) =>
+            <div className='data' key={index}>{ele + ":" + form[keys][ele]}</div>
+
+        )
         return (
             <React.Fragment>
                 <h1>Verify your Data</h1>
